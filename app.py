e7476e643916f5b73e90e7b25356efc7b4c01d1c
luiz.nunes <luiz.nunes@mprj.mp.br>
2025-02-17 18:08:01 -0300

    Final do Aula

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ from modelos.cardapio.prato import Prato
 restaurante = Restaurante('XFood', 'Japones')
 bebida_suco = Bebida('Suco de Limão', 5.0, 'grande')
 prato = Prato('Coxinha', 8, 'Coxinha com catupiry')
+restaurante.adicionar_no_cardapio(bebida_suco)
+restaurante.adicionar_no_cardapio(prato)
 
 
 
 
 def main():
-    print(bebida_suco)
-    print(prato)
+    restaurante.exibir_cardapio
+    prato.aplicar_desconto()
+    restaurante.exibir_cardapio
 
 if __name__ == '__main__':
     main ()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,6 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
+
 
 class Restaurante:
     restaurantes = []
@@ -7,6 +9,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -39,4 +42,31 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas/quantidade_de_notas, 1)
         return media
+    
+    
+    def adicionar_no_cardapio(self, item):
+        # Verificando se o item que estamos inserindo 
+        # for uma instancia do ItemCardapio ou herdar da mesma
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+     
+    @property        
+    def exibir_cardapio(self):
+        print (f'Cardapio do Restaurante {self._nome}\n')
+        #Enumerate devolve duas informações o index e o proprio item da lista
+        #O Start é usado para determinar qual o index do primeito item que por padrão é 0
+        for i,item in enumerate(self._cardapio, start=1):
+            mensagem = f'{i}. Nome:{item._nome} | Preço: R${item._preco}'
+            
+            if hasattr(item, '_descricao'):
+                mensagem += f' | Estoque: {item._descricao}'
+            else:
+                mensagem += f' | Tamanho: {item._tamanho}'
+            
+            print (mensagem)
+            
+        
+        
+
+        
         
